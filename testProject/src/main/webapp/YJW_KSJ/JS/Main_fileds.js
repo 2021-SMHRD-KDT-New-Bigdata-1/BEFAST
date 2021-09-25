a7ca7fc0920726edeb6a403e4d4635e21911b2c0
smhrd <smhrd@119.206.175.245>
2021-09-25 22:04:18 +0900

    -

--- testProject/src/main/webapp/YJW_KSJ/JS/Main_fileds.js
+++ testProject/src/main/webapp/YJW_KSJ/JS/Main_fileds.js
@@ -23,29 +23,3 @@ function categoryChange(e) {
     } 
 }
 
-
-
-// var subOptions = {
-//     rhkdtksrn : ['경기장1','경기장2','경기장3'],
-//     skarn : ['경기장4','경기장5','경기장6'],
-//     ehdrn : ['경기장7','경기장8','경기장9'],
-//     qnrrn : ['경기장10','경기장11','경기장12'],
-//     tjrn : ['경기장13','경기장14','경기장15']
-// }
-
-// switch (mainOption) {
-//     case '광산구' :
-//         var subOption = subOptions.rhkdtksrn;
-//         break;
-//     case '남구' :
-//         var subOption = subOptions.skarn;
-//         break;
-//     case '동구' :
-//         var subOption = subOptions.ehdrn;
-//         break;
-//     case '북구' :
-//         var subOption = subOptions.qnrrn;
-//         break;
-//     case '서구' :
-//         var subOption = subOptions.tjrn;
-// }
