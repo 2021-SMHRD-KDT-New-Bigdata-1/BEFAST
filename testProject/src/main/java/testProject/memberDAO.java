da02d0a81df17b2b0a90b0411fcce812ab66d0d5
smhrd <smhrd@119.206.175.245>
2021-09-25 19:32:13 +0900

    ID중복체크확인

---
+++ testProject/src/main/java/testProject/memberDAO.java
@@ -0,0 +1,81 @@
+package testProject;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class memberDAO {
+
+	   Connection conn = null;
+	   PreparedStatement psmt = null;
+	   ResultSet rs = null;
+	   
+	   public void conn() {
+		      
+		   try {
+			   Class.forName("oracle.jdbc.driver.OracleDriver");
+		         
+			   String url ="jdbc:oracle:thin:@project-db-stu.ddns.net:1524:xe";
+			   String dbid ="cgi_6_5";
+			   String dbpw ="smhrd5";
+			   conn = DriverManager.getConnection(url, dbid, dbpw);
+		         
+		   }catch(Exception e) {
+		         
+		   }
+		      
+		   }
+	   
+	   public void close() {
+		   try {
+			   if (rs!=null) {
+				   rs.close();
+			   }
+			   
+			   if (psmt!=null) {
+				   psmt.close();
+				   
+			   }if (conn!=null) {
+				   conn.close();
+			   }
+		         
+		   }catch(Exception e){
+			   e.printStackTrace();
+		   }
+	   }
+	   
+	   // 사용자가 입력한 MEMBER_ID 중복체크
+	   public boolean idCheck(String MEMBER_ID) {
+		   boolean check = false;
+		   conn();
+		   String sql = "select MEMBER_ID from MEMBERS where MEMBER_ID = ?";
+		   
+		   try {
+			   
+			   psmt = conn.prepareStatement(sql);
+			   psmt.setString(1, MEMBER_ID);
+			   rs = psmt.executeQuery(); // select문 쿼리
+			   
+			   if (rs.next()) { 
+				   // 사용자가 입력한 이메일이 테이블에 존재하는 경우
+				   check = true;
+			   }else { 
+				   // 사용자가 입력한 이메일이 테이블에 존재하지 않는 경우
+				   check = false;
+			   }
+			   
+		   }catch(Exception e) {
+			   e.printStackTrace();
+		   }finally {
+			   close();
+		   }
+		   
+		   return check;
+	   }
+	   
+	   
+	   
+	   
+	
+}
