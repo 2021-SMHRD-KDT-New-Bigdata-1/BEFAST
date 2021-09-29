6c78bc03e20ea96aa028d3bc16add8441bbb8580
smhrd <smhrd@121.147.185.22>
2021-09-29 16:44:53 +0900

    경기결과,평가항목 합치기

---
+++ testProject/src/main/java/newthang/MannerVO.java
@@ -0,0 +1,41 @@
+package newthang;
+
+public class MannerVO {
+	private String m_result;
+	private String time_result;
+	private String fair_palys;
+	private String languages;
+	private String comments;
+	public String getM_result() {
+		return m_result;
+	}
+	public void setM_result(String m_result) {
+		this.m_result = m_result;
+	}
+	public String getTime_result() {
+		return time_result;
+	}
+	public void setTime_result(String time_result) {
+		this.time_result = time_result;
+	}
+	public String getFair_palys() {
+		return fair_palys;
+	}
+	public void setFair_palys(String fair_palys) {
+		this.fair_palys = fair_palys;
+	}
+	public String getLanguages() {
+		return languages;
+	}
+	public void setLanguages(String languages) {
+		this.languages = languages;
+	}
+	public String getComments() {
+		return comments;
+	}
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+	
+	
+}
