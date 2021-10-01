2634dabfa79cb69d6bdcb1b4aa28e845433d3b1f
smhrd <smhrd@220.95.45.147>
2021-10-01 19:31:13 +0900

    ddddd

---
+++ testProject/src/main/java/newthang/TeamsssVO.java
@@ -0,0 +1,62 @@
+package newthang;
+
+public class TeamsssVO {
+	private int team_code;
+	private String team_name;
+	private int mannerscores;
+	private int winner_point; 
+	private String team_logo;
+	private String team_trophy;
+	private String team_info;
+	private String teamleader;
+	public int getTeam_code() {
+		return team_code;
+	}
+	public void setTeam_code(int team_code) {
+		this.team_code = team_code;
+	}
+	public String getTeam_name() {
+		return team_name;
+	}
+	public void setTeam_name(String team_name) {
+		this.team_name = team_name;
+	}
+	public int getMannerscores() {
+		return mannerscores;
+	}
+	public void setMannerscores(int mannerscores) {
+		this.mannerscores = mannerscores;
+	}
+	public int getWinner_point() {
+		return winner_point;
+	}
+	public void setWinner_point(int winner_point) {
+		this.winner_point = winner_point;
+	}
+	public String getTeam_logo() {
+		return team_logo;
+	}
+	public void setTeam_logo(String team_logo) {
+		this.team_logo = team_logo;
+	}
+	public String getTeam_trophy() {
+		return team_trophy;
+	}
+	public void setTeam_trophy(String team_trophy) {
+		this.team_trophy = team_trophy;
+	}
+	public String getTeam_info() {
+		return team_info;
+	}
+	public void setTeam_info(String team_info) {
+		this.team_info = team_info;
+	}
+	public String getTeamleader() {
+		return teamleader;
+	}
+	public void setTeamleader(String teamleader) {
+		this.teamleader = teamleader;
+	}
+
+	
+}
