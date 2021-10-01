cada044bcfb159283ec4f5467019282fe0facd16
smhrd <smhrd@119.206.175.245>
2021-10-01 15:09:23 +0900

    내정보 -> 예약현황

--- testProject/src/main/java/testProject/BookingService.java
+++ testProject/src/main/java/testProject/BookingService.java
@@ -20,24 +20,25 @@ public class BookingService extends HttpServlet {
       
       request.setCharacterEncoding("EUC-KR");
       
-      // 1. 사용자가 입력한 데이터 가져기
+      // 1. 사용자가 입력한 데이터 가져오기
       //String FIELD_CODE = request.getParameter("FIELD_CODE");
       String MATCHING_DATE = request.getParameter("MATCHING_DATE");
       String ADDRESS = request.getParameter("ADDRESS");
       String FILED_NAME = request.getParameter("FILED_NAME");
       String GAMES = request.getParameter("GAMES");
       String TIMES = request.getParameter("TIMES");
-      
+     
+      // 멤버 ID, 팀 name ?? 어디에서 받아야 하는가 --> 세션에 저장된 값을 불러온다!
       HttpSession session = request.getSession();
       memberVO vo = (memberVO)session.getAttribute("vo");
+      
+      // 세션에 있는 (로그인되어있는 아이디를 기준) 값을 들고온다.
       String TEAM_NAME = vo.getTEAM_NAME();
       String MEMBER_ID = vo.getMEMBER_ID();
       
       BookingDAO dao = new BookingDAO();
-//      HttpSession session   = request.getSession();
-//      memberVO vo = (memberVO)session.getAttribute("vo");
-//      String TEAM_NAME = vo.getTEAM_NAME();
       
+      // TEAM_NAME, MEMBER_ID는 MEMBERVO의 세션값을 들고온 것
       int cnt = dao.BOOKINGS(MATCHING_DATE, ADDRESS, FILED_NAME, GAMES, TIMES, TEAM_NAME, MEMBER_ID);
       
       
